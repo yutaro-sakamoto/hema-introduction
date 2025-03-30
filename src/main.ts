681f1aaaf986add844af20736686320aa25655d7
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-03-30 23:25:59 +0900

    feat: add an s3 bucket and a cloudfront distribution

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,41 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { App, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as cdk from "aws-cdk-lib";
 
-export class MyStack extends Stack {
+export class StaticWebsiteStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
+    // S3バケットの作成
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // スタック削除時にバケットを削除
+      autoDeleteObjects: true, // バケット内のオブジェクトも削除
+    });
 
-    // define resources here...
+    // CloudFrontディストリビューションの作成
+    const distribution = new cloudfront.Distribution(
+      this,
+      "WebsiteDistribution",
+      {
+        defaultRootObject: "index.html",
+        defaultBehavior: {
+          origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
+        },
+      },
+    );
+
+    // デプロイ後の出力
+    new cdk.CfnOutput(this, "BucketURL", {
+      value: websiteBucket.bucketWebsiteUrl,
+      description: "S3 Bucket URL",
+    });
+
+    new cdk.CfnOutput(this, "CloudFrontURL", {
+      value: distribution.distributionDomainName,
+      description: "CloudFront Distribution URL",
+    });
   }
 }
 
@@ -17,7 +47,7 @@ const devEnv = {
 
 const app = new App();
 
-new MyStack(app, 'hema-introduction-dev', { env: devEnv });
+new StaticWebsiteStack(app, "hema-introduction-dev", { env: devEnv });
 // new MyStack(app, 'hema-introduction-prod', { env: prodEnv });
 
 app.synth();
